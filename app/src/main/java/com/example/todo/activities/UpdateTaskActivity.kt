7d7209deffde566b4ec06d5d544625854d52cdae
SebastianDerendarz <Derendarz99@gmail.com>
2022-05-09 22:02:28 +0200

    Implement updateTask Layout

--- app/src/main/java/com/example/todo/activities/UpdateTaskActivity.kt
+++ app/src/main/java/com/example/todo/activities/UpdateTaskActivity.kt
@@ -1,7 +1,12 @@
 package com.example.todo.activities
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.room.Room
 import com.example.todo.DataObject
 import com.example.todo.Entity
@@ -10,16 +15,53 @@ import com.example.todo.ToDoDatabase
 import kotlinx.android.synthetic.main.activity_update_task.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
 
-class UpdateTaskActivity : ActivityBase() {
+class UpdateTaskActivity : ActivityBase(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
     private lateinit var database: ToDoDatabase
+    private var year = 0
+    private var month = 0
+    private var day = 0
+    private var hour = 0
+    private var minute = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_task)
         database = Room.databaseBuilder(
             applicationContext, ToDoDatabase::class.java, "To_Do"
         ).build()
+
+        val taskCategories = resources.getStringArray(R.array.TaskCategories)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, taskCategories)
+        updateCategoryInputText.setAdapter(arrayAdapter)
+
+        getCurrentCalendarDateTime()
+        updateTaskTimeView()
+
+        updateTaskTimeInput.setOnClickListener {
+            getCurrentCalendarDateTime()
+            DatePickerDialog(this, this, year, month, day).show()
+        }
+
+        updateNotifyLayoutInput.setOnClickListener {
+            if (updateNotifyLayoutInput.text.toString() == "Notify"){
+                updateNotifyLayoutInput.setText("Muted")
+            } else {
+                updateNotifyLayoutInput.setText("Notify")
+            }
+        }
+
+        updateStatusLayoutInput.setOnClickListener {
+            if (updateStatusLayoutInput.text.toString() == "Pending"){
+                updateStatusLayoutInput.setText("Done")
+            } else {
+                updateStatusLayoutInput.setText("Pending")
+            }
+        }
+
         val pos = intent.getIntExtra("id", -1)
         if (pos != -1) {
             val title = DataObject.getData(pos).title
@@ -64,5 +106,52 @@ class UpdateTaskActivity : ActivityBase() {
     private fun myIntent() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun getCurrentCalendarDateTime() {
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("CET"))
+        year = calendar.get(Calendar.YEAR)
+        month = calendar.get(Calendar.MONTH)
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        hour = calendar.get(Calendar.HOUR_OF_DAY)
+        minute = calendar.get(Calendar.MINUTE)
+    }
+
+    private fun updateTaskTimeView() {
+        val monthWithLeadingZero = if (month < 10) {
+            "0$month"
+        } else {
+            month.toString()
+        }
+        val dayWithLeadingZero = if (day < 10) {
+            "0$day"
+        } else {
+            day.toString()
+        }
+        val hourWithLeadingZero = if (hour < 10) {
+            "0$hour"
+        } else {
+            hour.toString()
+        }
+        val minuteWithLeadingZero = if (minute < 10) {
+            "0$minute"
+        } else {
+            minute.toString()
+        }
+        updateTaskTimeInput.setText("$year-$monthWithLeadingZero-$dayWithLeadingZero $hourWithLeadingZero:$minuteWithLeadingZero")
+    }
+
+    override fun onDateSet(view: DatePicker?, yearVal: Int, monthVal: Int, dayOfMonthVal: Int) {
+        year = yearVal
+        month = monthVal
+        day = dayOfMonthVal
+        updateTaskTimeView()
+        TimePickerDialog(this, this, hour, minute, true).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDayVal: Int, minuteVal: Int) {
+        hour = hourOfDayVal
+        minute = minuteVal
+        updateTaskTimeView()
     }
 }
