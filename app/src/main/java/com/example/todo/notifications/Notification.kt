ab906453ca4cd9ef3a2dddf3bb3d3057c8d8abe8
SebastianDerendarz <Derendarz99@gmail.com>
2022-05-20 19:23:48 +0200

    add opening of updateTaskActivity when clicked on notification

--- app/src/main/java/com/example/todo/notifications/Notification.kt
+++ app/src/main/java/com/example/todo/notifications/Notification.kt
@@ -1,11 +1,13 @@
 package com.example.todo.notifications
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.example.todo.R
+import com.example.todo.activities.UpdateTaskActivity
 
 
 const val channelId = "dueDateChannel"
@@ -17,10 +19,17 @@ const val titleExtra = "titleExtra"
 class Notification : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        val resultIntent = Intent(context, UpdateTaskActivity::class.java)
+        resultIntent.putExtra("taskId", intent.getIntExtra(notificationIdExtra, 1))
+        val resultPendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         val notification = NotificationCompat.Builder(context, channelId)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(intent.getStringExtra(titleExtra))
             .setContentText(intent.getStringExtra(messageExtra))
+            .setAutoCancel(true)
+            .setContentIntent(resultPendingIntent)
             .build()
         val notificationId = intent.getIntExtra(notificationIdExtra, 0)
 
